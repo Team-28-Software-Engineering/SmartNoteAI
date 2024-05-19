c352aa30b88e52e7e71c3fd4296fddcdf1b6a009
Nguyễn Việt Bắc <127400120+bacngv@users.noreply.github.com>
2024-05-19 08:00:12 +0700

    update about.py

--- about.py
+++ about.py
@@ -44,4 +44,4 @@ class Ui_Dialog(object):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "About"))
         self.label.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><img src=\":/res/icons/text-editor.png\"/></p><p align=\"center\">Smart Note AI</p></body></html>"))
-        self.label_2.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt;\">Author : Team-28-Software-Engineering</span></p><p align=\"center\"><span style=\" font-size:12pt;\">Build Date : 15-5-2024</span></p><p align=\"center\"><span style=\" font-size:12pt;\">Version : 1.0</span></p></body></html>"))
+        self.label_2.setText(_translate("Dialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt;\">Author : Team-28-Software-Engineering</span></p><p align=\"center\"><span style=\" font-size:12pt;\">Build Date : 15-5-2024</span></p><p align=\"center\"><span style=\" font-size:12pt;\">Version : 2.0</span></p></body></html>"))
